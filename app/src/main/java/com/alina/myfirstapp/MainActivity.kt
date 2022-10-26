5b41aab24196c14b4f1bf338c5c0af3e01b9b25c
FlyingKiwi09 <alinahookway@gmail.com>
2022-10-27 08:50:25 +1300

    initial commit

---
+++ app/src/main/java/com/alina/myfirstapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.alina.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val myTextView = findViewById<TextView>(R.id.textView2)
+        var timesClicked = 0
+        btnClickMe.setOnClickListener {
+            timesClicked += 1
+            myTextView.text = timesClicked.toString()
+
+
+        }
+    }
+}
